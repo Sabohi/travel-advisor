3348b144bdab388c9668552d6ddd15f49afb9ea7
sabohi <sabuhizaidi616@gmail.com>
2024-08-20 00:47:13 +0530

    added missing import

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-// import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "../styles/Home.module.css";
 import dynamic from "next/dynamic";
+import Link from 'next/link';
 
 const Nav = dynamic(() => import('subscriberApp/nav'));
 const Footer = dynamic(() => import('subscriberApp/footer'));
